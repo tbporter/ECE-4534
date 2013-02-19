e3eb4fe01e8b4186a8c83687302f594a0d940a7c
tbporter <travis42@vt.edu>
2013-02-19 01:10:58 -0500

    add webserver stuff

---
+++ ARMCode/RTOSDemo/MainFiles/g9_webTask.h
@@ -0,0 +1,33 @@
+#ifndef __WEB_TASK_H__
+#define __WEB_TASK_H__
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "projdefs.h"
+#include "semphr.h"
+
+#include "vtUtilities.h"
+#include "vtI2C.h"
+#include "messages.h"
+
+typedef struct __webStruct {
+	vtI2CStruct *i2c; //Needed for sending messages out
+	xQueueHandle inQ; //Queue for incoming messages
+} webStruct;
+
+// Public API
+//
+// Start the task
+// Args:
+//   webData: Data structure used by the task
+//   uxPriority -- the priority you want this task to be run at
+//   i2c: pointer to the data structure for an i2c task
+void vStartNavigationTask(webStruct* webData,unsigned portBASE_TYPE uxPriority, vtI2CStruct *i2c);
+
+//Sends a message to the web task
+//	msg -- a pointer to a variable of type g9Msg
+//	ticksToBlock -- how long the routine should wait if the queue is full
+//	Return: Result of the call to xQueueSend()
+portBASE_TYPE SendWebMsg(webStruct* web,g9Msg* msg,portTickType ticksToBlock);
+#endif
