683765c5de76249a1046a989c747f1aa1a5b4969
Shaishav Parekh <parekhsm@vt.edu>
2013-03-23 21:48:19 -0400

    Added Working slave and master I2C under PICFrameworkOriginal

---
+++ PICCode/PICFrameworkOriginal/PICFrameworkWindows/src/maindefs.h
@@ -0,0 +1,43 @@
+#ifndef __maindefs
+#define __maindefs
+
+#ifdef __XC8
+#include <xc.h>
+#ifdef _18F45J10
+#define __USE18F45J10 1
+#else
+#ifdef _18F2680
+#define __USE18F2680 1
+#endif
+#endif
+#else
+#ifdef __18F45J10
+#define __USE18F45J10 1
+#else
+#ifdef __18F2680
+#define __USE18F2680 1
+#endif
+#endif
+#include <p18cxxx.h>
+#endif
+
+// Message type definitions
+#define MSGT_TIMER0 10
+#define MSGT_TIMER1 11
+#define MSGT_MAIN1 20
+#define	MSGT_OVERRUN 30
+#define MSGT_UART_DATA 31
+#define MSGT_I2C_DBG 41
+#define	MSGT_I2C_DATA 40
+#define MSGT_I2C_RQST 42
+#define MSGT_I2C_MASTER_SEND_COMPLETE 43
+#define MSGT_I2C_MASTER_SEND_FAILED 44
+#define MSGT_I2C_MASTER_RECV_COMPLETE 45
+#define MSGT_I2C_MASTER_RECV_FAILED 46
+#define MSGT_POLL_PICS 13
+
+#define MASTERPIC 1
+//#define SLAVEPIC 1
+
+#endif
+
---
+++ PICCode/PICFrameworkOriginal/PICFrameworkWindows/src/my_i2c.h
@@ -0,0 +1,49 @@
+#ifndef __my_i2c_h
+#define __my_i2c_h
+
+#include "messages.h"
+
+#define MAXI2CBUF MSGLEN
+typedef struct __i2c_comm {
+	unsigned char buffer[MAXI2CBUF];
+	unsigned char	buflen;
+	unsigned char	event_count;
+	unsigned char	status;
+	unsigned char	error_code;
+	unsigned char	error_count;
+	unsigned char outbuffer[MAXI2CBUF];
+	unsigned char outbuflen;
+	unsigned char outbufind;
+	unsigned char slave_addr;
+} i2c_comm;
+
+#define I2C_STOP 0x4
+#define I2C_IDLE 0x5
+#define I2C_STARTED 0x6
+#define	I2C_RCV_DATA 0x7
+#define I2C_SLAVE_SEND 0x8
+#define I2C_MASTER_SEND 0x9
+#define I2C_MASTER_RECV 0XA
+#define I2C_MASTER_SEND_ADDR 0xB
+#define I2C_MASTER_RECV_ADDR 0xC
+#define I2C_MASTER_DATA_READ 0xD
+
+#define I2C_ERR_THRESHOLD 1
+#define I2C_ERR_OVERRUN 0x4
+#define I2C_ERR_NOADDR 0x5
+#define I2C_ERR_NODATA 0x6
+#define I2C_ERR_MSGTOOLONG 0x7
+#define I2C_ERR_MSG_TRUNC 0x8
+
+void init_i2c(i2c_comm *);
+void i2c_int_handler(void);
+void i2c_master_handler(void);
+void i2c_slave_handler(void);
+void start_i2c_slave_reply(unsigned char,unsigned char *);
+void i2c_configure_slave(unsigned char);
+void i2c_configure_master(unsigned char);
+unsigned char i2c_master_send(unsigned char,unsigned char *);
+unsigned char i2c_master_recv(unsigned char, unsigned char);
+//unsigned char i2c_master_recv_cmd(unsigned char, unsigned char, unsigned char);
+
+#endif
